6c3cea5dd1930731e7cef0ba40058a236e1638d3
Milan Sevak <milansevak194@gmail.com>
2026-04-27 13:01:55 +0530

    Fixed start command for Render

--- run.py
+++ run.py
@@ -74,9 +74,9 @@ def yajman():
 def payment():
     return render_template("payment.html")
 
-
 # ==============================
 # 🚀 RUN (FOR RENDER)
 # ==============================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
